49c85211ff2523cd276ba4fc4a9981e6e61fa017
Kancharla Lakshmi vyshnavi <vyshuv474@gmail.com>
2026-05-23 12:59:51 +0530

    Update few_shot.py

--- few_shot.py
+++ few_shot.py
@@ -9,7 +9,7 @@ class FewShotPosts:
         self.load_posts(file_path)
 
     def load_posts(self, file_path):
-    with open(file_path, encoding="utf-8", errors="ignore") as f:
+        with open(file_path, encoding="utf-8", errors="ignore") as f:
         posts = json.load(f)
 
     self.df = pd.json_normalize(posts)
