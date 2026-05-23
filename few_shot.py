921b1bed3fbb868cddb5dffc59f414852389842d
Kancharla Lakshmi vyshnavi <vyshuv474@gmail.com>
2026-05-23 13:03:21 +0530

    Update few_shot.py

--- few_shot.py
+++ few_shot.py
@@ -11,26 +11,26 @@ class FewShotPosts:
     def load_posts(self, file_path):
         with open(file_path, encoding="utf-8", errors="ignore") as f:
             posts = json.load(f)
-        
 
-    self.df = pd.json_normalize(posts)
+        self.df = pd.json_normalize(posts)
 
-    self.df['length'] = self.df['line_count'].apply(
-        self.categorize_length
-    )
+        self.df["length"] = self.df["line_count"].apply(
+            self.categorize_length
+        )
 
-    # collect unique tags
-    all_tags = self.df['tags'].apply(lambda x: x).sum()
+        # collect unique tags
+        all_tags = self.df["tags"].apply(lambda x: x).sum()
 
-    self.unique_tags = list(set(all_tags))
+        self.unique_tags = list(set(all_tags))
 
     def get_filtered_posts(self, length, language, tag):
         df_filtered = self.df[
-            (self.df['tags'].apply(lambda tags: tag in tags)) &  # Tags contain 'Influencer'
-            (self.df['language'] == language) &  # Language is 'English'
-            (self.df['length'] == length)  # Line count is less than 5
+            (self.df["tags"].apply(lambda tags: tag in tags))
+            & (self.df["language"] == language)
+            & (self.df["length"] == length)
         ]
-        return df_filtered.to_dict(orient='records')
+
+        return df_filtered.to_dict(orient="records")
 
     def categorize_length(self, line_count):
         if line_count < 5:
@@ -46,6 +46,5 @@ class FewShotPosts:
 
 if __name__ == "__main__":
     fs = FewShotPosts()
-    # print(fs.get_tags())
-    posts = fs.get_filtered_posts("Medium","Hinglish","Job Search")
+    posts = fs.get_filtered_posts("Medium", "Hinglish", "Job Search")
     print(posts)
